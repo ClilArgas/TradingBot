30a9d558efef32d0c6ff5f115e10ab41b9a644d8
Clil Argas <clilargas23@gmail.com>
2022-01-03 10:44:30 +0200

    Changed Functionaliity of checkBuySellSignals and built stop loss and take profit

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const exchange = new ccxt.binance({
   secret: process.env.API_SECRET,
 });
 exchange.createOrder;
-let candles, hlc, orderDetails, shortTrend, longTrend, inPosition = false;
+let candles,
+  hlc,
+  orderDetails,
+  shortTrend,
+  longTrend,
+  inPosition = false;
 const init = async () => {
   candles = await exchange.fetchOHLCV('ETH/USDT', '1h');
   //candle[0] = date, candle[1] = openPrice, candle[2]=highPrice, candle[3]=lowestPrice, candle[4]=closePrice, candle[5]= volume, candle[6]=uptrend?
@@ -39,55 +44,155 @@ main();
 const checkBuySellSignals = async (shortTrend, longTrend) => {
   //taking the last two candles and checking if there was a change in the trend and giving buy/sell signals & checking if the longTrend is an uptrend
   const shortCrucialField = shortTrend.slice(-3, -1);
-  const longCrucialField = longTrend.slice(-2, -1);
-  if (longCrucialField) {
-    if (!shortCrucialField[0] && shortCrucialField[1]) {
-      if (!inPosition) {
-        orderDetails = await orders.makeOrder('buy', exchange, 100, candles);
-        inPosition = true;
-      }
+  const longCrucialField = longTrend.slice(-3, -1);
+  if (
+    (longCrucialField[1] && !shortCrucialField[0] && shortCrucialField[1]) ||
+    (shortCrucialField[1] && !longCrucialField[0] && longCrucialField[1])
+  ) {
+    if (!inPosition) {
+      orderDetails = await orders.makeOrder('buy', exchange, 100, candles);
+      inPosition = true;
     }
-    if (shortCrucialField[0] && !shortCrucialField[1]) {
-      if (inPosition) {
-        await exchange.createMarketOrder(
-          'ETH/USDT',
-          'sell',
-          orderDetails.orderAmount
-        );
-        orderDetails = undefined;
-        inPosition = false;
-      }
-    }
-    return;
   }
-  if (shortCrucialField[0] && !shortCrucialField[1]) {
+  if (
+    (shortCrucialField[0] && !shortCrucialField[1]) ||
+    (longCrucialField[0] && !longCrucialField[1])
+  ) {
+    if (inPosition) {
+      const order = await exchange.createMarketOrder(
+        'ETH/USDT',
+        'sell',
+        orderDetails.orderAmount
+      );
+      console.log(order);
+      orderDetails = undefined;
+      inPosition = false;
+    }
+  }
+  if (
+    (!longCrucialField[1] && shortCrucialField[0] && !shortCrucialField[1]) ||
+    (!shortCrucialField[1] && longCrucialField[0] && !longCrucialField[1])
+  ) {
     if (!inPosition) {
       orderDetails = await orders.makeOrder('sell', exchange, 100, candles);
       inPosition = true;
     }
   }
-  if (!shortCrucialField[0] && shortCrucialField[1]) {
+  if (
+    (!shortCrucialField[0] && shortCrucialField[1]) ||
+    (!longCrucialField[0] && longCrucialField[1])
+  ) {
     if (inPosition) {
-      await exchange.createMarketOrder(
+      const order = await exchange.createMarketOrder(
         'ETH/USDT',
         'buy',
         orderDetails.orderAmount
       );
+      console.log(order);
+      orderDetails = undefined;
+      inPosition = false;
+    }
+  }
+  // if (longCrucialField) {
+  //   if (!shortCrucialField[0] && shortCrucialField[1]) {
+  //     if (!inPosition) {
+  //       orderDetails = await orders.makeOrder('buy', exchange, 100, candles);
+  //       inPosition = true;
+  //     }
+  //   }
+  //   if (shortCrucialField[0] && !shortCrucialField[1]) {
+  //     if (inPosition) {
+  //       await exchange.createMarketOrder(
+  //         'ETH/USDT',
+  //         'sell',
+  //         orderDetails.orderAmount
+  //       );
+  //       orderDetails = undefined;
+  //       inPosition = false;
+  //     }
+  //   }
+  //   return;
+  // }
+  // if (shortCrucialField[0] && !shortCrucialField[1]) {
+  //   if (!inPosition) {
+  //     orderDetails = await orders.makeOrder('sell', exchange, 100, candles);
+  //     inPosition = true;
+  //   }
+  // }
+  // if (!shortCrucialField[0] && shortCrucialField[1]) {
+  //   if (inPosition) {
+  //     await exchange.createMarketOrder(
+  //       'ETH/USDT',
+  //       'buy',
+  //       orderDetails.orderAmount
+  //     );
+  //     orderDetails = undefined;
+  //     inPosition = false;
+  //   }
+  // }
+};
+const checkSLTP = async (orderDetails) => {
+  if (!inPosition) return;
+  if (orderDetails.orderSide === 'buy') {
+    const sl = orderDetails.orderPrice * 0.985;
+    const tp = orderDetails.orderPrice * 1.03;
+    const currPrice = candles[candles.length - 1][4];
+    if (currPrice >= tp) {
+      const order = await exchange.createMarketOrder(
+        'ETH/USDT',
+        'sell',
+        orderDetails.orderAmount
+      );
+      console.log(order);
+      orderDetails = undefined;
+      inPosition = false;
+    }
+    if (currPrice <= sl) {
+      const order = await exchange.createMarketOrder(
+        'ETH/USDT',
+        'sell',
+        orderDetails.orderAmount
+      );
+      console.log(order);
+      orderDetails = undefined;
+      inPosition = false;
+    }
+  }
+  if (orderDetails.orderSide === 'sell') {
+    const sl = orderDetails.orderPrice * 1.015;
+    const tp = orderDetails.orderPrice * 0.97;
+    const currPrice = candles[candles.length - 1][4];
+    if (currPrice <= tp) {
+      const order = await exchange.createMarketOrder(
+        'ETH/USDT',
+        'buy',
+        orderDetails.orderAmount
+      );
+      console.log(order);
+      orderDetails = undefined;
+      inPosition = false;
+    }
+    if (currPrice >= sl) {
+      const order = await exchange.createMarketOrder(
+        'ETH/USDT',
+        'buy',
+        orderDetails.orderAmount
+      );
+      console.log(order);
       orderDetails = undefined;
       inPosition = false;
     }
   }
 };
-const checkSLTP = (shortTrend, longTrend) => {
-  const shortCrucialField = shortTrend.slice(-3, -1);
-  const longCrucialField = longTrend.slice(-2, -1);
-};
 const rule = new schedule.RecurrenceRule();
 rule.second = 1;
-// const fetchBars = schedule.scheduleJob(rule, async () => {
-//   await main();
-//   checkBuySellSignals(candles);
-// });
+const fetchBars = schedule.scheduleJob(
+  '0,5,10,15,20,25,30,35,40,45,50,55 * * * * *',
+  async () => {
+    await main();
+    console.log(candles[candles.length - 1][4]);
+  }
+);
 
 process.on('SIGINT', () => {
   schedule.gracefulShutdown().then(() => process.exit(0));
